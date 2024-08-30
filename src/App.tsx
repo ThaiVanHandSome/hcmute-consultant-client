06dbf139ccdb68fca6047baca3798e79fdb772e1
ThaiVanHandSome <danitbadao1234@gmail.com>
2024-08-30 18:06:18 +0700

    create react environment (eslint, prettier, ...) and install shadcnUI library

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Search from '@/components/dev/Search'
+import { Button } from '@/components/ui/button'
+
+function App() {
+  return (
+    <>
+      <Button>Xin chao</Button>
+      <Search />
+    </>
+  )
+}
+
+export default App
